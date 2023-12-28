a2791210b7ce25f52a3e0db2806078c5422d7fcc
rotembreslauer <rotem.breslauer@post.runi.ac.il>
2023-12-28 18:59:39 +0200

    add OneOfEach

--- OneOfEach.java
+++ OneOfEach.java
@@ -1,15 +1,20 @@
-
-/**
- *  Simulates the formation of a family in which the parents decide 
- *  to have children until they have at least one child of each gender.
- */
-public class OneOfEach {
+public class OneOfEachStats1 {
 	public static void main (String[] args) {
-		
-		String gender1 = "0";
+	
+	int T = Integer.parseInt(args[0]);
+		int seed = Integer.parseInt(args[1]);
+        Random generator = new Random(seed);  
+
+        int f2 = 0;
+        int f3 = 0;
+        int f4 = 0;
+        String gender1 = "0";
 		String gender2 = "1";
 		boolean flag = true;
-		int counter = 0;
+     	int counter = 0;
+
+
+        for(int i = 0; i < T ; i ++){
 
 		do{
 			double j = Math.random();
@@ -19,24 +24,51 @@ public class OneOfEach {
 				gender1 = "g";
 			} else {
 				gender1 = "b";
-
 			}
-
-			System.out.print(gender1 + " ");
 
 			if((gender1 != gender2) && (counter > 1)){
 				flag = false;
 
-			System.out.println();
-			System.out.println("You made it... and you now have " + counter + " children");
-			}	
+			if (counter == 2){
+				f2 = f2 + 1;
+			  } else {
+			    if (counter == 3){
+				f3 = f3 +1;
+			  } else { 
+				f4 = f4 +1;
 
+			}
+		}
+			}
 			gender2 = gender1;
 
 		} while (flag);
-			
+
+		flag = true;
+		counter = 0;
+		}	
+		
+	
+		double average = (((f2*2) + (f3*3) + (f4*4))/(double) T);
+		String common = "0";
+
+		if ((f2 > f3) && (f2 > f4)){
+		common = "2";
+   		 } else if (f3 > f2 && f3 > f4){
+		common = "3";
+		} else {
+		common = "4 or more";
+	}
+
+
+		System.out.println("Average: " + average + " children to get at least one of each gender.");
+		System.out.println("Number of families with 2 children: " + f2);
+		System.out.println("Number of families with 3 children: " + f3);
+		System.out.println("Number of families with 4 or more children: " + f4);
+		System.out.println("The most common number of children is " + common);		
 	}
 }
-	
+
 
 	
+
