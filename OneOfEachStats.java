5cbd97ebaa7f188568c751c9c003211f1bc38d5b
rotembreslauer <rotem.breslauer@post.runi.ac.il>
2023-12-31 19:37:40 +0200

    last

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -1,35 +1,26 @@
 import java.util.Random;
-/**
- *  Computes some statistics about families in which the parents decide 
- *  to have children until they have at least one child of each gender.
- *  The program expects to get one command-line argument: an int value
- * 	that determines how many families to simulate.
- */
+
 public class OneOfEachStats{
 	public static void main (String[] args) {
 	
 		int T = Integer.parseInt(args[0]);
 		int seed = Integer.parseInt(args[1]);
 		Random generator = new Random(seed);
-         
-
         int f2 = 0;
         int f3 = 0;
         int f4 = 0;
         int moreThen4 = 0;
-        String gender1 = "0";
-		String gender2 = "1";
-		boolean flag = true;
-     	int counter = 0;
-
+     
 
         for(int i = 0; i < T ; i ++){
-        	double j = generator.nextDouble();
+        	String gender1 = "0";
+			String gender2 = "1";
+			int counter = 0;
+        
 			
 		do{
-			 j = generator.nextDouble();
-			counter += 1;
-
+				double j = generator.nextDouble();
+			
 
 			if(j >= 0.5){
 				gender1 = "g";
@@ -37,31 +28,34 @@ public class OneOfEachStats{
 				gender1 = "b";
 			}
 
+			counter += 1;
+
 			if((gender1 != gender2) && (counter > 1)){
-				flag = false;
+				break;
+
 			}
 		
 			gender2 = gender1;
-		
-		} while (flag);
 
+		
+		} while (true);
 
 			if (counter == 2){
-				f2 = f2 + 1;
+				f2 ++;
 			  } else {
 			    if (counter == 3){
-				f3 = f3 +1;
+				f3 ++;
 			  } else { 
-				f4 = f4 +1;
+			  	if (counter > 3){
+				f4 ++;
 				moreThen4 = moreThen4 + counter;
+			}
 		}
-		flag = true;
-		counter = 0;
 		}	
 	}
 
 
-	double average = (((f2*2) + (f3*3) + (moreThen4))/(double) T);
+	double average = ((double)((f2*2) + (f3*3) + (moreThen4))/ T);
 		String common = "0";
 
 		if ((f2 > f3) && (f2 > f4)){
